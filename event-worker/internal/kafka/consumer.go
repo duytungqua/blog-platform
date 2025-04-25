fb0054c708bd1972f9df9f36e623f02535321995
duytungqua <quangduy.hcmut@gmail.com>
2025-04-25 15:32:58 +0700

    feat: test read kafka

--- event-worker/internal/kafka/consumer.go
+++ event-worker/internal/kafka/consumer.go
@@ -7,5 +7,18 @@ import (
 )
 
 func StartConsumer(broker, topic, groupID string) error{
-	
+	kafkaConsumer, err := kafka.NewConsumer(broker, topic, groupID)
+	if err != nil {
+		return fmt.Errorf("failed to create consumer: %w", err)
+	}
+	defer kafkaConsumer.Close()
+	for {		
+		message, err := kafkaConsumer.Consume()
+		if err != nil {
+			log.Printf("Error consuming message: %v", err)
+			continue
+		}
+		log.Printf("Received message: %s", message)
+	}
+	return nil
 }
